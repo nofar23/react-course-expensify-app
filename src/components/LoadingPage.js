facdca6792ce550bfb8083e107c1af1bd5548346
nofar23 <nofarreshef10@gmail.com>
2018-11-27 08:58:15 +0200

    Add Expensify styles

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LoadinPage = () => (
+    <div className="loader">
+        <img className="loader__image" src="/images/loader.gif"/>
+    </div>
+);
+
+export default LoadinPage;
+
